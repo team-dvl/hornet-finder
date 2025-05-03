f494d140724249f909d431af16ee7389024ad198
Eric Devolder <eric.devolder@gmail.com>
2025-05-03 16:31:46 +0000

    certbot enabled

--- auth/docker-entrypoint.sh
+++ auth/docker-entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 if [ "$KC_HOSTNAME" = "0.0.0.0:8080" ]; then
   echo "Starting in DEV mode..."
-  exec /opt/keycloak/bin/kc.sh start-dev --db-url "jdbc:postgresql://hornet-finder-keycloak-db/keycloak" --db postgres --db-username keycloak --db-password "$KC_DB_PASSWORD" --hostname "localhost" --hostname-strict=false --http-relative-path /auth
+  exec /opt/keycloak/bin/kc.sh start-dev --db-url "jdbc:postgresql://hornet-finder-keycloak-db/keycloak" --db postgres --db-username keycloak --db-password "$KC_DB_PASSWORD" --hostname "localhost" --hostname-strict=false
 
 else
   echo "Starting in PROD mode..."
-  exec /opt/keycloak/bin/kc.sh start --optimized --http-relative-path /auth
+  exec /opt/keycloak/bin/kc.sh start --optimized --http-enabled=true
 fi
