4dab415de35c697a867c169cc8d6a53183e7e060
Eric Devolder <eric.devolder@gmail.com>
2025-07-06 15:13:39 +0000

    added openID client react component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,41 @@
 import './App.css'
 import { Col, Container, Row } from 'react-bootstrap'
 import CompassMap from './CompassMap'
+import { useAuth } from 'react-oidc-context';
 
 function App() {
 
+  const auth = useAuth();
+
+  switch (auth.activeNavigator) {
+      case "signinSilent":
+          return <div>Signing you in...</div>;
+      case "signoutRedirect":
+          return <div>Signing you out...</div>;
+  }
+
+  if (auth.isLoading) {
+      return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+      return <div>Oops... {auth.error.name} caused {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+      return (
+      <div>
+          Hello {auth.user?.profile.sub}{" "}
+          <button onClick={() => void auth.removeUser()}>Log out</button>
+      </div>
+      );
+
+}
 
   return (
     <Container fluid className="px-3">
       <h2>Hornet Nest Finder</h2>
-      <p>Find and report hornet nests in your area. </p>
+      <p>Find and report hornet nests in your area.</p>
     
       <Row className="w-100">
         <Col xs={12} className="w-100">
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from 'react-oidc-context'
 import './index.css'
 import App from './App'
 
+const oidcConfig = {
+  authority: 'https://auth.velutina.ovh/realms/hornet-finder',
+  client_id: 'hornet-app',
+  redirect_uri: window.location.origin,
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App/>
+    <AuthProvider {...oidcConfig}>
+      <App/>
+    </AuthProvider>
   </StrictMode>,
 )
