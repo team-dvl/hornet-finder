34f7dd3e8302d2a2b55b9101c55d4a6e423658f4
Eric Devolder <eric.devolder@gmail.com>
2025-07-09 02:32:50 +0000

    small aesthetic changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,38 +9,36 @@ function App() {
   const auth = useAuth();
 
   switch (auth.activeNavigator) {
-      case "signinSilent":
-          return <div>Signing you in...</div>;
-      case "signoutRedirect":
-          return <div>Signing you out...</div>;
+    case "signinSilent":
+      return <div>Signing you in...</div>;
+    case "signoutRedirect":
+      return <div>Signing you out...</div>;
   }
 
   if (auth.isLoading) {
-      return <div>Loading...</div>;
+    return <div>Loading...</div>;
   }
 
   if (auth.error) {
-      return <div>Oops... {auth.error.name} caused {auth.error.message}</div>;
+    return <div>Oops... {auth.error.name} caused {auth.error.message}</div>;
   }
 
 
   return (
-    <>
-      <Container fluid className="px-3">
+      <Container fluid className='px-0'>
         <h2>Hornet Nest Finder</h2>
-        <p>Find and report hornet nests in your area!</p>
-        { /* login button, display only if not authenticated */ }
-        { !auth.isAuthenticated && <button onClick={() => void auth.signinRedirect()}>Log in</button> }
-        { auth.isAuthenticated && <button onClick={() => void auth.signoutRedirect(
+        <p><i>Find and report hornet nests in your area!</i></p>
+        { /* login button, display only if not authenticated */}
+        {!auth.isAuthenticated && <button onClick={() => void auth.signinRedirect()}>Log in</button>}
+        {auth.isAuthenticated && <button onClick={() => void auth.signoutRedirect(
           { post_logout_redirect_uri: window.location.origin }
-        )}>Log out {auth.user?.profile.name}</button> }
-        <Row className="w-100">
+        )}>Log out {auth.user?.profile.name}</button>}
+        <Row className="w-100 g-0">
           <Col xs={12} className="w-100">
-            <InteractiveMap/>
+            <InteractiveMap />
           </Col>
         </Row>
       </Container>
-    </>
   )
 }
 
