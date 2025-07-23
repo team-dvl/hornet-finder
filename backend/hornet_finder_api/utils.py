b64a77f95061cd897551739403c9feec5561f980
mdevolde <martin.devolder2@gmail.com>
2025-07-23 16:16:06 +0000

    fix: removing useless permission from hornet-api client (user management) and removing useless linked func

--- backend/hornet_finder_api/utils.py
+++ backend/hornet_finder_api/utils.py
@@ -53,27 +53,3 @@ def user_exists(email: str) -> bool:
     """
     keycloak_admin = _get_keycloak_admin()
     return keycloak_admin.get_user_id(email) is not None
-
-def assign_beekeeper_role(email: str):
-    """
-    Assigns the 'beekeeper' role to a user with the given email.
-    If the user does not exist, it raises an exception.
-
-    :param email: The email of the user to assign the role to.
-    :type email: str
-
-    :raises ValueError: If the user does not exist or the role does not exist.
-    """
-    keycloak_admin = _get_keycloak_admin()
-    user_id = keycloak_admin.get_user_id(email)
-    
-    if not user_id:
-        raise ValueError(f"User with email {email} does not exist.")
-    
-    roles = keycloak_admin.get_realm_roles()
-    role = next((role for role in roles if role['name'] == 'beekeeper'), None)
-    
-    if not role:
-        raise ValueError("Role 'beekeeper' does not exist.")
-    
-    keycloak_admin.assign_realm_roles(user_id=user_id, roles=[role])
