9e881fe658156ccb0421c2ce6eec5cf0e1645c76
Eric Devolder <eric.devolder@gmail.com>
2025-07-28 01:57:03 +0000

    fix: ajust paths to ColorSelector from forms.

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -1,6 +1,6 @@
+export { default as ColorSelector } from './ColorSelector';
 export { default as InfestationLevelInput } from './InfestationLevelInput';
 export type { InfestationLevel } from './InfestationLevelInput';
-export { default as ColorSelector } from './ColorSelector';
 export type { ColorSelectorProps } from './ColorSelector';
 export { default as CoordinateInput } from './CoordinateInput';
 export type { CoordinateInputProps } from './CoordinateInput';
--- frontend/src/components/forms/index.ts
+++ frontend/src/components/forms/index.ts
@@ -1,1 +1,2 @@
 export { default as AddItemSelector } from './AddItemSelector';
+export * from '../common';
